3e4609406871b43edc74abc536bf01420f2f069f
claudev89 <menaresclaudio08@gmail.com>
2024-09-20 00:22:50 -0300

    Aplicación localizada en español, inclutendo Jetstream. Gracias a Laravel Lang. También se arreglaron los botones de las páginas por defecto en corcondancia con la paleta de colores.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,4 @@
 @extends('layouts.app')
-<button type="button" class="btn btn-primary">Hola</button>
-<button type="button" class="btn btn-secondary">Chao</button>
+@section('content')
+    Hola
+@endsection
