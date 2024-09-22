7e19afff2b3415aa404f45d82f7f961dbb1aea6a
claudev89 <menaresclaudio08@gmail.com>
2024-09-22 01:41:55 -0300

    Header terminado

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -47,21 +47,26 @@
                                     <label class="form-check-label" for="remember">
                                         {{ __('Remember Me') }}
                                     </label>
+
+                                    @if (Route::has('password.request'))
+                                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                                            {{ __('Forgot Your Password?') }}
+                                        </a>
+                                    @endif
                                 </div>
                             </div>
                         </div>
 
                         <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary text-white">
+                                <button type="submit" class="btn btn-primary text-white me-2">
                                     {{ __('Login') }}
                                 </button>
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
+                                <a type="button" class="btn btn-secondary" href="{{ route('register') }}">
+                                    {{ __('Register') }}
+                                </a>
+
                             </div>
                         </div>
                     </form>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,20 +18,12 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm mb-1 py-0">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
+                <div>
+                    <a href="#" class="text-reset" style="text-decoration: none"><strong>Contáctate con nosotros</strong></a>
+                </div>
+                <div>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
@@ -39,13 +31,9 @@
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
-                                    <a class="nav-link text-dark" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link text-dark" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link text-dark" href="{{ route('login') }}">
+                                        <i class="bi bi-person" style="-webkit-text-stroke: 1px"></i><strong> {{ __('Login') }}</strong>
+                                    </a>
                                 </li>
                             @endif
                         @else
@@ -67,7 +55,41 @@
                                 </div>
                             </li>
                         @endguest
+                        <li class="nav-item">
+                            <a class="nav-link text-dark">
+                                <i class="bi bi-cart3" style="-webkit-text-stroke: 1px" href="#"></i><strong> Carrito (0)</strong>
+                            </a>
+                        </li>
                     </ul>
+                </div>
+            </div>
+        </nav>
+
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    <img src="logo.png" alt="{{ config('app.name', 'Laravel') }}" title="{{ config('app.name', 'Laravel') }}">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
+                        <li class="nav-item"><a href="#" class="nav-link text-dark text-uppercase"><strong>Máquinas</strong></a> </li>
+                        <li class="nav-item"><a href="#" class="nav-link text-dark text-uppercase"><strong>Productos para el cabello</strong></a></li>
+                        <li class="nav-item"><a href="#" class="nav-link text-dark text-uppercase"><strong>Cepillos y peinetas</strong></a></li>
+                        <li class="nav-item"><a href="#" class="nav-link text-dark text-uppercase"><strong>Afeitado</strong></a></li>
+                    </ul>
+
+
+                </div>
+                <div>
+                    <div class="input-group">
+                        <span class="input-group-text" id="search-icon"><i class="bi bi-search"></i></span>
+                        <input type="text" class="form-control" placeholder="Buscar en el catálogo" aria-label="Buscar" aria-describedby="search-icon">
+                    </div>
                 </div>
             </div>
         </nav>
